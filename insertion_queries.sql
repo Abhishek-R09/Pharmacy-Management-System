49e82feff32d613a0bc39a2390e598eb200339bd
Siddharth Gandhi <ssgandhi602@gmail.com>
2020-10-11 17:58:21 +0530

    started insertion

---
+++ insertion_queries.sql
@@ -0,0 +1,53 @@
+
+-- Inserting into doctor_1
+INSERT INTO `pharmacy_management_system`.`doctor_1`(`doc_name`,`contact`,`specialization`)
+VALUES ("Abhishek Ramgirkar","1234567890","Cardiology");
+INSERT INTO `pharmacy_management_system`.`doctor_1`(`doc_name`,`contact`,`specialization`)
+VALUES ("Ranjit Gandhi","4567890123","Radiology");
+INSERT INTO `pharmacy_management_system`.`doctor_1`(`doc_name`,`contact`,`specialization`)
+VALUES ("Siddharth Modi","1237890456","Opthalmology");
+INSERT INTO `pharmacy_management_system`.`doctor_1`(`doc_name`,`contact`,`specialization`)
+VALUES ("Kamla Harris","1278903456","Oncology");
+INSERT INTO `pharmacy_management_system`.`doctor_1`(`doc_name`,`contact`,`specialization`)
+VALUES ("Rahul Gandhi","5678901234","Neurology");
+
+
+-- drug manufacturer
+INSERT INTO `pharmacy_management_system`.`drug_manufacturer`(`name`,`contact`)
+VALUES("Ranbaxy","6029111180");
+INSERT INTO `pharmacy_management_system`.`drug_manufacturer`(`name`,`contact`)
+VALUES("Sun Pharmaceutical","7873274667");
+INSERT INTO `pharmacy_management_system`.`drug_manufacturer`(`name`,`contact`)
+VALUES("Cipla Limited","9375326206");
+INSERT INTO `pharmacy_management_system`.`drug_manufacturer`(`name`,`contact`)
+VALUES("Abbott","3436587348");
+INSERT INTO `pharmacy_management_system`.`drug_manufacturer`(`name`,`contact`)
+VALUES("Dr. Reddy’s Laboratories","8116889621");
+INSERT INTO `pharmacy_management_system`.`drug_manufacturer`(`name`,`contact`)
+VALUES("Cadila Healthcare","8738706469");
+INSERT INTO `pharmacy_management_system`.`drug_manufacturer`(`name`,`contact`)
+VALUES("Biocon Limited","5192738457");
+
+-- patient_1
+
+INSERT INTO `pharmacy_management_system`.`patient_1`(`pat_name`,`contact`,`gender`,`insurance_id`,`age`,`address`)
+VALUES("Balaji Chandra","4404258095","Male","CDR8RTSSC9",42,"22, Charu Heights, Hadapsar Bhubhaneshwar - 140503");
+INSERT INTO `pharmacy_management_system`.`patient_1`(`pat_name`,`contact`,`gender`,`insurance_id`,`age`,`address`)
+VALUES("Yadu Sagar","9934489221","Male","8CQQJB96AU",12,"22, Charu Heights, Hadapsar Bhubhaneshwar - 140503");
+INSERT INTO `pharmacy_management_system`.`patient_1`(`pat_name`,`contact`,`gender`,`insurance_id`,`age`,`address`)
+VALUES("Hari Mukhopadhyay","9167616923","Male","3Q9SX9DI0K",54,"61, Chhavi Chowk, Kanpur - 486638");
+INSERT INTO `pharmacy_management_system`.`patient_1`(`pat_name`,`contact`,`gender`,`insurance_id`,`age`,`address`)
+VALUES("Madhu Kar","1665735754","Female","K5FKGCVD2B",16,"42, Suresh Nagar, Meerut - 129993");
+INSERT INTO `pharmacy_management_system`.`patient_1`(`pat_name`,`contact`,`gender`,`insurance_id`,`age`,`address`)
+VALUES("Pradeep Rao Biyani","5867510581","Others","LOUCDEK5AC",69,"78, Ramesh Villas, ViratGarh Gandhinagar - 531145");
+INSERT INTO `pharmacy_management_system`.`patient_1`(`pat_name`,`contact`,`gender`,`insurance_id`,`age`,`address`)
+VALUES("Radhika Buch","4950601987","Female","V29bill_1PMYCYQW",31,"76, Kormangala, Kanpur - 299187");
+INSERT INTO `pharmacy_management_system`.`patient_1`(`pat_name`,`contact`,`gender`,`insurance_id`,`age`,`address`)
+VALUES("Sabina Chauhan","7758898172","Female","1M1FGY3J1C",28,"40, Naina Society, Satish Nagar Surat - 577788");
+INSERT INTO `pharmacy_management_system`.`patient_1`(`pat_name`,`contact`,`gender`,`insurance_id`,`age`,`address`)
+VALUES("Balaji Vyas ","9214536446","Male","E2Z9VIOLLF",92,"28, Tushar Heights, SahilPur Pune - 154228");
+INSERT INTO `pharmacy_management_system`.`patient_1`(`pat_name`,`contact`,`gender`,`insurance_id`,`age`,`address`)
+VALUES("Mridula Tabeed Shetty ","8664052988","Female","SJ7AZU5N01",45,"56, Hadapsar, Jaipur - 232155");
+INSERT INTO `pharmacy_management_system`.`patient_1`(`pat_name`,`contact`,`gender`,`insurance_id`,`age`,`address`)
+VALUES("Meghana Ratan Thaker","5384456729","Female","GA23Z79269",38,"78, Chirag Chowk, Noida - 194914");
+
