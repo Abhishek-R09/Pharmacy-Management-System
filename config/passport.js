1dcfd5213ace92d4f44e0c7c5edd7767f4a3ed4e
Siddharth Gandhi <ssgandhi602@gmail.com>
2020-10-15 11:12:13 +0530

    Remove signup page

--- config/passport.js
+++ config/passport.js
@@ -40,44 +40,44 @@ module.exports = function (passport) {
     // we are using named strategies since we have one for login and one for signup
     // by default, if there was no name, it would just be called 'local'
 
-    passport.use(
-        'local-signup',
-        new LocalStrategy({
-            // by default, local strategy uses username and password, we will override with email
-            usernameField: 'username',
-            passwordField: 'password',
-            passReqToCallback: true // allows us to pass back the entire request to the callback
-        },
-            function (req, username, password, done) {
-                // find a user whose email is the same as the forms email
-                // we are checking to see if the user trying to login already exists
-                // connection.connect();
-                connection.query("SELECT * FROM login WHERE username = ?", [username], function (err, rows) {
-                    if (err)
-                        return done(err);
-                    if (rows.length) {
-                        return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
-                    } else {
-                        // if there is no user with that username
-                        // create the user
-                        var newUserMysql = {
-                            username: username,
-                            password: bcrypt.hashSync(password, null, null)  // use the generateHash function in our user model
-                        };
+    // passport.use(
+    //     'local-signup',
+    //     new LocalStrategy({
+    //         // by default, local strategy uses username and password, we will override with email
+    //         usernameField: 'username',
+    //         passwordField: 'password',
+    //         passReqToCallback: true // allows us to pass back the entire request to the callback
+    //     },
+    //         function (req, username, password, done) {
+    //             // find a user whose email is the same as the forms email
+    //             // we are checking to see if the user trying to login already exists
+    //             // connection.connect();
+    //             connection.query("SELECT * FROM login WHERE username = ?", [username], function (err, rows) {
+    //                 if (err)
+    //                     return done(err);
+    //                 if (rows.length) {
+    //                     return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
+    //                 } else {
+    //                     // if there is no user with that username
+    //                     // create the user
+    //                     var newUserMysql = {
+    //                         username: username,
+    //                         password: bcrypt.hashSync(password, null, null)  // use the generateHash function in our user model
+    //                     };
 
-                        var insertQuery = "INSERT INTO login ( username, password, role) values (?,?, 'Admin')";
-                        // connection.connect();
-                        connection.query(insertQuery, [newUserMysql.username, newUserMysql.password], function (err, rows) {
-                            newUserMysql.id = rows.insertId;
+    //                     var insertQuery = "INSERT INTO login ( username, password, role) values (?,?, 'Admin')";
+    //                     // connection.connect();
+    //                     connection.query(insertQuery, [newUserMysql.username, newUserMysql.password], function (err, rows) {
+    //                         newUserMysql.id = rows.insertId;
 
-                            return done(null, newUserMysql);
-                        });
-                        // connection.end();
-                    }
-                });
-                // connection.end();
-            })
-    );
+    //                         return done(null, newUserMysql);
+    //                     });
+    //                     // connection.end();
+    //                 }
+    //             });
+    //             // connection.end();
+    //         })
+    // );
 
     // =========================================================================
     // LOCAL LOGIN =============================================================
