a8fd35667d738c705bc4da6f05d28e393251fd9f
Siddharth Gandhi <ssgandhi602@gmail.com>
2020-10-20 09:40:52 +0530

    remove signup page

--- app/routes.js
+++ app/routes.js
@@ -47,17 +47,17 @@ module.exports = function (app, passport) {
 	// SIGNUP ==============================
 	// =====================================
 	// show the signup form
-	app.get('/signup', function (req, res) {
-		// render the page and pass in any flash data if it exists
-		res.render('signup.ejs', { message: req.flash('signupMessage') });
-	});
+	// app.get('/signup', function (req, res) {
+	// 	// render the page and pass in any flash data if it exists
+	// 	res.render('signup.ejs', { message: req.flash('signupMessage') });
+	// });
 
 	// process the signup form
-	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect: '/home', // redirect to the secure profile section
-		failureRedirect: '/signup', // redirect back to the signup page if there is an error
-		failureFlash: true // allow flash messages
-	}));
+	// app.post('/signup', passport.authenticate('local-signup', {
+	// 	successRedirect: '/home', // redirect to the secure profile section
+	// 	failureRedirect: '/signup', // redirect back to the signup page if there is an error
+	// 	failureFlash: true // allow flash messages
+	// }));
 
 	// =====================================
 	// PROFILE SECTION =========================
@@ -167,8 +167,10 @@ module.exports = function (app, passport) {
 function isLoggedIn(req, res, next) {
 
 	// if user is authenticated in the session, carry on
-	if (req.isAuthenticated())
+	if (req.isAuthenticated()){
+		// res.redirect('/home');
 		return next();
+	}
 
 	// if they aren't redirect them to the home page
 	res.redirect('/');
