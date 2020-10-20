3d2b728f4551f47ce5a212b73a8ee8793653de67
Abhishek911cse <61340365+Abhishek911cse@users.noreply.github.com>
2020-10-20 09:56:13 +0530

    Change route

--- app/routes.js
+++ app/routes.js
@@ -56,19 +56,11 @@ module.exports = function (app, passport) {
 	// });
 
 	// process the signup form
-<<<<<<< HEAD
 	// app.post('/signup', passport.authenticate('local-signup', {
 	// 	successRedirect: '/home', // redirect to the secure profile section
 	// 	failureRedirect: '/signup', // redirect back to the signup page if there is an error
 	// 	failureFlash: true // allow flash messages
 	// }));
-=======
-	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect: '/', // redirect to the secure profile section
-		failureRedirect: '/signup', // redirect back to the signup page if there is an error
-		failureFlash: true // allow flash messages
-	}));
->>>>>>> route-change
 
 	// =====================================
 	// PROFILE SECTION =========================
