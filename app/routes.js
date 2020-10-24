a9922ff4aa84c2df078fe0aa43a9b4252d6fcb25
Abhishek911cse <61340365+Abhishek911cse@users.noreply.github.com>
2020-10-25 00:11:38 +0530

    Change inventory and doctor route

--- app/routes.js
+++ app/routes.js
@@ -280,6 +280,19 @@ module.exports = function (app, passport) {
 		// res.render("doctor_details.ejs", {user: req.user});
 	});
 
+	app.post("/addDoctor", function(req,res){
+		var docName = req.body.doctor_name;
+		var docContact = req.body.doctor_contact;
+		var docSpecialization = req.body.doctor_specialization;
+		var query = "INSERT INTO doctor_1 (doc_name, contact, specialization) VALUES (?,?,?)";
+		connection.query(query, [docName, docContact, docSpecialization], function(err, rows){
+			if (err) {
+				console.log(err);
+			}
+			res.redirect('/doctors');
+		});
+	});
+
 	app.get("/inventory", isLoggedIn, function(req, res){
 		var query = "SELECT medicine.med_id, medicine.med_name, medicine.mrp, medicine.primary_drug, \
 		drug_manufacturer.name \
@@ -291,13 +304,54 @@ module.exports = function (app, passport) {
 			from inventory INNER JOIN \
 			medicine ON inventory.med_id=medicine.med_id ORDER BY medicine.med_id;";
 			connection.query(query1, function(err, rows1){
-				res.render('inventory.ejs', {user: req.user, rows: rows, rows1: rows1});
+				var query3 = "SELECT employee.emp_name, employee.emp_id, login.role \
+				FROM employee inner join login ON employee.username=login.username";
+				connection.query(query3, function(err,rows2){
+					var query4 = "select * from drug_manufacturer";
+					connection.query(query4, function(err, rows3){
+						res.render('inventory.ejs', {user: req.user, rows: rows, rows1: rows1, rows2: rows2, rows3: rows3});				
+					});
+				});
 			});
 			// res.render('inventory.ejs', {user: req.user, rows: rows});
 		});
 		// res.render("inventory.ejs", {user: req.user});
 	});
 
+	app.post("/addStock", function(req,res){
+		var medId = req.body.med_id;
+		var expiryDate = req.body.expiry_date;
+		var totalStock = req.body.total_stock;
+		var associatedEmpId = req.body.associated_emp_id;
+		console.log(medId);
+		console.log(expiryDate);
+		console.log(totalStock);
+		console.log(associatedEmpId);
+		var query = "INSERT INTO inventory (med_id, expiry_date, total_number, emp_id) \
+		VALUES (?,?,?,?)";
+		connection.query(query, [medId, expiryDate, totalStock, associatedEmpId], function(err,rows){
+			if (err) {
+				console.log(err);
+			}
+			res.redirect('/inventory');
+		});
+	});
+
+	app.post('/addMedicine', function(req,res){
+		var medName = req.body.new_med_name;
+		var medMRP = req.body.new_med_mrp;
+		var primaryDrug = req.body.primary_drug;
+		var dosage = req.body.dosage;
+		var companyId = req.body.company_id;
+		var query = "INSERT INTO medicine (med_name, mrp, primary_drug, dosage, company_id) \
+		VALUES (?,?,?,?,?)";
+		connection.query(query, [medName, medMRP, primaryDrug, dosage, companyId], function(err,rows){
+			if (err) {
+				console.log(err);
+			}
+			res.redirect('/inventory');
+		});
+	});
 	// app.get("/inventory", isLoggedIn, function(req, res){
 	// 	res.render()
 	// });
